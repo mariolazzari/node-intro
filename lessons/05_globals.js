e3cf3783d6913c1be1a4f11dc3d90980859fa6a1
Mario Lazzari <mario.lazzari@gmail.com>
2021-12-25 21:19:36 +0100

    Done lesson 5

---
+++ lessons/05_globals.js
@@ -0,0 +1,9 @@
+// global object
+console.log("Global obj:", global);
+// current dir
+console.log("Current dir:", __dirname);
+// current file
+console.log("Current file:", __filename);
+
+// process object
+console.log("\nProcess obj:", process);
