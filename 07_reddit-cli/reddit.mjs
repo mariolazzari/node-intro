d8d1e8472845a41c0c7d385a1588d4a9ce9df8df
Mario Lazzari <mario.lazzari@gmail.com>
2021-12-29 15:40:59 +0100

    Done lesson 14

--- 07_reddit-cli/reddit.mjs
+++ 07_reddit-cli/reddit.mjs
@@ -1,3 +1,19 @@
 #! /usr/bin/env node
+import yargs from "yargs";
 
-console.log("Reddit CLI");
+import fetch from "node-fetch";
+import open from "open";
+
+const { argv } = yargs(process.argv);
+const res = await fetch(`https://www.reddit.com/.json`);
+const data = await res.json();
+const { children } = data.data;
+const randomPost = children[Math.floor(Math.random() * children.length)];
+const link = "https://reddit.com" + randomPost.data.permalink;
+
+if (argv.print) {
+  console.log(randomPost.data.title);
+  console.log(link);
+} else {
+  open(link);
+}
